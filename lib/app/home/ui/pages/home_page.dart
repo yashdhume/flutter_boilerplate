81f62cf63f185cf805b744fac5540286b07450a6
Yash Dhume <yash.dhume@gmail.com>
2023-12-24 17:13:54 -0800

    feat(debug): added token copy to clipboard

--- lib/app/home/ui/pages/home_page.dart
+++ lib/app/home/ui/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:frontend/app/authentication/logic/providers/auth_providers.dart';
+import 'package:frontend/app/authentication/logic/service/token_service.dart';
 import 'package:frontend/common/utils/language.dart';
 
 class HomePage extends ConsumerWidget {
@@ -9,11 +11,20 @@ class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: ref.read(authServiceProvider).signOut,
-          child: Text(Language.text.signOut),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () async => Clipboard.setData(
+              ClipboardData(text: (await TokenService.getToken()) ?? ''),
+            ),
+            child: const Text('Get Token'),
+          ),
+          ElevatedButton(
+            onPressed: ref.read(authServiceProvider).signOut,
+            child: Text(Language.text.signOut),
+          ),
+        ],
       ),
     );
   }
